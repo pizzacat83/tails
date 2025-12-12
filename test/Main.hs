24653bfb63c6b15b3b110921dd40f30a5d433785
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2025-12-13 00:24:33 +0900

    tidy up

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,9 @@
 module Main (main) where
 
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain $ testGroup "tails"
+    [ testCase "sanity" $ 1 @?= (1 :: Int)
+    ]
